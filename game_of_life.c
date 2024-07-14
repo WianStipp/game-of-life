2c4780883b3e9cde56c40fd3fccb2698319ea4f1
Wian Stipp <stippwian@gmail.com>
2024-07-13 18:26:45 -0700

    cuda single step working

---
+++ game_of_life.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <time.h>
+
+#define NUM_ROWS 10
+#define NUM_COLS 10
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+
+
+int count_neighbors(int a[NUM_ROWS][NUM_COLS], int x, int y) {
+    int i,j;
+    int sum = 0;
+    for (i=-1;i<2;i++) {
+        for (j=-1;j<2;j++) {
+            if ((x+i >= 0) && (x+i < NUM_ROWS) && (y+j >= 0) && (y+j<NUM_COLS)) {
+                sum = sum + a[x+i][y+j];
+            }
+        }
+    }
+    return sum - a[x][y];
+}
+
+void update_next_state(int a[NUM_ROWS][NUM_COLS], int next_state[NUM_ROWS][NUM_COLS]) {
+    int x, y, num_neighbors;
+    for (x=0;x<NUM_ROWS;x++) {
+        for (y=0;y<NUM_COLS;y++) {
+            num_neighbors = count_neighbors(a, x, y);
+            if (num_neighbors < 2) {
+                next_state[x][y] = 0;
+            }
+            else if (num_neighbors == 3) {
+                next_state[x][y] = 1;
+            }
+            else {
+                next_state[x][y] = a[x][y];
+            }
+        }
+    }
+}
+
+void set_blinker(int a[NUM_ROWS][NUM_COLS]) {
+    a[5][4] = 1;
+    a[5][5] = 1;
+    a[5][6] = 1;
+}
+
+void set_blank_state(int a[NUM_ROWS][NUM_COLS]) {
+    int i,j;
+    for (i=0;i<NUM_ROWS;i++) {
+        for (j=0;j<NUM_COLS;j++) {
+            a[i][j] = 0;
+        }
+    }
+}
+
+void render_state(int a[NUM_ROWS][NUM_COLS]) {
+    int i,j;
+    for (i=0;i<NUM_ROWS;i++) {
+        for (j=0;j<NUM_COLS;j++) {
+            printf("%d   ", a[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void set_state_as_next(int a[NUM_ROWS][NUM_COLS], int b[NUM_ROWS][NUM_COLS]) {
+    int i,j;
+    for (i=0;i<NUM_ROWS;i++) {
+        for (j=0;j<NUM_COLS;j++) {
+            a[i][j] = b[i][j];
+        }
+    }
+}
+
+
+int main(void) {
+    int state[NUM_ROWS][NUM_COLS];
+    int next_state[NUM_ROWS][NUM_COLS];
+    int i;
+    float start_time, end_time;
+    set_blank_state(state);
+    set_blinker(state);
+    render_state(state);
+
+    start_time = (float)clock()/CLOCKS_PER_SEC;
+    for (i=0;i<1000000;i++) {
+        update_next_state(state, next_state);
+        // render_state(next_state);
+        set_state_as_next(state, next_state);
+        // printf("\n");
+    }
+    end_time = (float)clock()/CLOCKS_PER_SEC;
+    printf("Simulation took %f seconds\n", end_time-start_time);
+
+    return 0;
+}
--- game_of_life.cu
+++ game_of_life.cu
@@ -1,34 +1,16 @@
 #include <stdio.h>
 
-#define NUM_ROWS 10
-#define NUM_COLS 10
+#define GRID_SIZE 10
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
-__global__
-void add(int a, int b, int *c) {
-    *c = a + b;
-}
-
-int cuda_test(void) {
-    int c;
-    int *dev_c;
-    cudaMalloc( (void**) &dev_c, sizeof(int) );
-    add<<<1,1>>>(2, 7, dev_c);
-    // cudaDeviceSynchronize();
-    cudaMemcpy(&c, dev_c, sizeof(int), cudaMemcpyDeviceToHost);
-    printf("2+7=%d\n", c);
-    cudaFree(dev_c);
-    return 0;
-}
-
-
-int count_neighbors(int a[NUM_ROWS][NUM_COLS], int x, int y) {
+__device__
+int count_neighbors(int a[GRID_SIZE][GRID_SIZE], int x, int y) {
     int i,j;
     int sum = 0;
     for (i=-1;i<2;i++) {
         for (j=-1;j<2;j++) {
-            if ((x+i >= 0) & (x+i < NUM_ROWS) & (y+j >= 0) & (y+j<NUM_COLS)) {
+            if ((x+i >= 0) && (x+i < GRID_SIZE) && (y+j >= 0) && (y+j<GRID_SIZE)) {
                 sum = sum + a[x+i][y+j];
             }
         }
@@ -36,56 +18,53 @@ int count_neighbors(int a[NUM_ROWS][NUM_COLS], int x, int y) {
     return sum - a[x][y];
 }
 
-void update_next_state(int a[NUM_ROWS][NUM_COLS], int next_state[NUM_ROWS][NUM_COLS]) {
-    int x, y, num_neighbors;
-    for (x=0;x<NUM_ROWS;x++) {
-        for (y=0;y<NUM_COLS;y++) {
-            // printf("(%d, %d)\n", x,y);
-            // printf("Num neighbors: %d\n", count_neighbors(a, x, y));
-            num_neighbors = count_neighbors(a, x, y);
-            if (num_neighbors < 2) {
-                next_state[x][y] = 0;
-            }
-            else if (num_neighbors == 3) {
-                next_state[x][y] = 1;
-            }
-            else {
-                next_state[x][y] = a[x][y];
-            }
-        }
+__global__
+void update_next_state(int a[][GRID_SIZE], int next_state[][GRID_SIZE]) {
+    int x = threadIdx.x;
+    int y = threadIdx.y;
+    int num_neighbors;
+    num_neighbors = count_neighbors(a, x, y);
+    if (num_neighbors < 2) {
+        next_state[x][y] = 0;
+    }
+    else if (num_neighbors == 3) {
+        next_state[x][y] = 1;
+    }
+    else {
+        next_state[x][y] = a[x][y];
     }
 }
 
 
-void set_blinker(int a[NUM_ROWS][NUM_COLS]) {
+void set_blinker(int a[GRID_SIZE][GRID_SIZE]) {
     a[5][4] = 1;
     a[5][5] = 1;
     a[5][6] = 1;
 }
 
-void set_blank_state(int a[NUM_ROWS][NUM_COLS]) {
+void set_blank_state(int a[GRID_SIZE][GRID_SIZE]) {
     int i,j;
-    for (i=0;i<NUM_ROWS;i++) {
-        for (j=0;j<NUM_COLS;j++) {
+    for (i=0;i<GRID_SIZE;i++) {
+        for (j=0;j<GRID_SIZE;j++) {
             a[i][j] = 0;
         }
     }
 }
 
-void render_state(int a[NUM_ROWS][NUM_COLS]) {
+void render_state(int a[GRID_SIZE][GRID_SIZE]) {
     int i,j;
-    for (i=0;i<NUM_ROWS;i++) {
-        for (j=0;j<NUM_COLS;j++) {
+    for (i=0;i<GRID_SIZE;i++) {
+        for (j=0;j<GRID_SIZE;j++) {
             printf("%d   ", a[i][j]);
         }
         printf("\n");
     }
 }
 
-void set_state_as_next(int a[NUM_ROWS][NUM_COLS], int b[NUM_ROWS][NUM_COLS]) {
+void set_state_as_next(int a[GRID_SIZE][GRID_SIZE], int b[GRID_SIZE][GRID_SIZE]) {
     int i,j;
-    for (i=0;i<NUM_ROWS;i++) {
-        for (j=0;j<NUM_COLS;j++) {
+    for (i=0;i<GRID_SIZE;i++) {
+        for (j=0;j<GRID_SIZE;j++) {
             a[i][j] = b[i][j];
         }
     }
@@ -93,18 +72,24 @@ void set_state_as_next(int a[NUM_ROWS][NUM_COLS], int b[NUM_ROWS][NUM_COLS]) {
 
 
 int main(void) {
-    int state[NUM_ROWS][NUM_COLS];
-    int next_state[NUM_ROWS][NUM_COLS];
     int i;
+    int state[GRID_SIZE][GRID_SIZE];
+    int next_state[GRID_SIZE][GRID_SIZE];
+    dim3 grid(GRID_SIZE, GRID_SIZE);
+    int (*dev_state)[GRID_SIZE], (*dev_next_state)[GRID_SIZE];
 
     set_blank_state(state);
     set_blinker(state);
     render_state(state);
-    for (i=0;i<10;i++) {
-        update_next_state(state, next_state);
-        render_state(next_state);
-        set_state_as_next(state, next_state);
-        printf("\n");
-    }
+    cudaMalloc((void**) &dev_state, sizeof(int)*GRID_SIZE*GRID_SIZE);
+    cudaMalloc((void**) &dev_next_state, sizeof(int)*GRID_SIZE*GRID_SIZE);
+
+    cudaMemcpy(dev_state, state, sizeof(int)*GRID_SIZE*GRID_SIZE, cudaMemcpyHostToDevice);
+    update_next_state<<<1,grid>>>(dev_state, dev_next_state);
+
+    cudaMemcpy(next_state, dev_next_state, sizeof(int)*GRID_SIZE*GRID_SIZE, cudaMemcpyDeviceToHost);
+    render_state(next_state);
+    cudaFree(dev_state);
+    cudaFree(dev_next_state);
     return 0;
 }
